cf550f593487202c98d41f0d40764f252c1f9924
Harif3562 <148810485+HarifSR@users.noreply.github.com>
2025-10-23 22:50:13 -0600

    Prueba 2

--- cab/src/App.jsx
+++ cab/src/App.jsx
@@ -9,36 +9,30 @@ import UserManagement from './pages/admin/UserManagement';
 import SurveyList from './pages/Surveyor/SurveyList';
 import AppLayout from './components/layout/AppLayout';
 import DataViewer from './pages/Surveyor/DataViewer';
-// ...otras vistas...
+import DataAnalytics from './pages/admin/DataAnalytics'; // ← AGREGAR ESTA LÍNEA
 
 const App = () => {
     return (
         <Routes>
-
             <Route path="/" element={<Login />} />
-            {/* Rutas Públicas (Login) */}
             <Route path="/login" element={<Login />} />
-            {/* ... */}
 
             {/* RUTAS PROTEGIDAS PARA ADMINISTRADOR */}
             <Route element={<ProtectedRoute allowedRoles={['admin']} />}>
-                <Route element={<AppLayout />}> {/* Usa AppLayout */}
+                <Route element={<AppLayout />}>
                     <Route path="/admin/dashboard" element={<DashboardAdmin />} />
                     <Route path="/admin/users" element={<UserManagement />} />
-                    {/* ... */}
+                    <Route path="/admin/analytics" element={<DataAnalytics />} /> {/* ← AGREGAR ESTA LÍNEA */}
                 </Route>
             </Route>
 
             {/* RUTAS PROTEGIDAS PARA ENCUESTADOR */}
             <Route element={<ProtectedRoute allowedRoles={['surveyor', 'admin']} />}>
-                <Route element={<AppLayout />}> {/* Usa AppLayout */}
+                <Route element={<AppLayout />}>
                     <Route path="/surveyor/list" element={<SurveyList />} />
                     <Route path="/surveyor/viewer" element={<DataViewer />} />
-                    {/* ... */}
                 </Route>
             </Route>
-            
-            {/* ... */}
         </Routes>
     );
 };
